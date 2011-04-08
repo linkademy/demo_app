6aa7fa22347d7dc6f3eb96ad99412da2ebc8d199
linkademy <rvasavada@linkademy.com>
2011-04-08 18:00:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c1b41e4f00b5f42bbfaf452580fbfbe510c5fb6eec4d37bf75678214faddc1275ac615053df5570b1896a4a5722565db758d75729ad88a09f83bde0de92b1617'
